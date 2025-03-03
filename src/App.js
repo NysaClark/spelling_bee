6f89cb39e8ab9ce0e0427ba86a3011212f761d48
Nysa <naominysa@gmail.com>
2025-03-03 15:04:35 -0700

    temp letter & pangram fetch

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import Loading from "./Components/Loading";
 import Game from "./Components/Game";
 import Score from "./Components/Score";
 
+import gamesList from "./games.json";
+import pangramsList from "./pangrams.json";
+
 function App() {
   const [showStart, setShowStart] = useState(true);
   const [showGame, setShowGame] = useState(false);
   const [showScore, setShowScore] = useState(false);
+
+  const [letters, setLetters] = useState([]);
+  const [middle, setMiddle] = useState("");
+  const [dictionary, setDictionary] = useState([]);
+  const [pangrams, setPangrams] = useState([]);
 
   const [loading, setLoading] = useState(false);
 
@@ -16,6 +24,12 @@ function App() {
     setShowStart(false);
     setLoading(true);
 
+    const randomGame = gamesList[Math.floor(Math.random() * 17589)]
+  
+    setLetters(randomGame.char)
+    setMiddle(randomGame.middle)
+    setDictionary(randomGame.dictionary)
+    setPangrams(pangramsList[randomGame.id.split("_")[0]]);
 
     setShowGame(true);
     setLoading(false);
