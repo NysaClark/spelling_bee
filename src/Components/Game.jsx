3126b1bbf2424d7ebf0dc89a994faffcc7020628
Nysa <nys2116834@maricopa.edu>
2025-03-03 20:43:48 -0700

    gameboard hive

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -4,13 +4,16 @@ import WordList from './Game/WordList'
 import Board from './Game/Board'
 
 const Game = () => {
-  const [currRank, setCurrRank] = useState("genius");
-  const [currPoints, setCurrPoints] = useState(65);
+  const [currRank, setCurrRank] = useState("beginner");
+  const [currPoints, setCurrPoints] = useState(1);
 
   const [expanded, setExpanded] = useState(false);
 
   const [wordsFound, setWordsFound] = useState([
   ])
+
+  const [cLetter, setCLetter] = useState("p")
+  const [letters, setLetters] = useState(["i","k","c","d","e","n"])
 
   return (
     <div id='game'>
@@ -19,7 +22,7 @@ const Game = () => {
         <WordList expanded={expanded} setExpanded={setExpanded} wordsFound={wordsFound} />
       </div>
       <div className="section">
-        <Board />
+        <Board cLetter={cLetter} letters={letters} setLetters={setLetters} />
       </div>
     </div>
   )
--- src/Components/Game/Board.jsx
+++ src/Components/Game/Board.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Board = () => {
+const Board = ({ cLetter, letters, setLetters }) => {
+  const [userInput, setUserInput] = useState("");
+
+  const refresh = () => {
+    const temp = [...letters];
+    for (let i = temp.length - 1; i > 0; i--) {
+      const rand = Math.floor(Math.random() * (i + 1));
+      [temp[i], temp[rand]] = [temp[rand], temp[i]];
+    }
+    return temp;
+  }
+//   function shuffleArr (array){
+//     for (var i = array.length - 1; i > 0; i--) {
+//         var rand = Math.floor(Math.random() * (i + 1));
+//         [array[i], array[rand]] = [array[rand], array[i]]
+//     }
+// }
+
+  // const handleInput = (e) => {
+  //   // e.preventDefault()
+  //   console.log(e.target.value);
+  //   if (e.target.value.trim() !== "") {
+  //     let lastLetter =
+  //       e.target.value.length &&
+  //       e.target.value[e.target.value.length - 1].toLowerCase();
+  //     console.log(lastLetter);
+  //     if (cLetter === lastLetter || letters.includes(lastLetter)) {
+  //       setUserInput((prev) => (prev += lastLetter));
+  //     }
+  //     // else {
+  //     //   lastLetter = "";
+  //     // }
+  //   }
+  // };
+
   return (
-    <div id='board'>Board</div>
-  )
-}
+    <div id="board">
+      <input
+        id="inputBox"
+        type="text"
+        value={userInput}
+        readOnly
+        // onChange={(e) => handleInput(e)}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <div className="grid">
+        <div id="hive">
+          <div
+            onClick={() => setUserInput((prev) => prev + cLetter)}
+            className="hive-cell center"
+          >
+            {cLetter}
+          </div>
+          {letters.map((letter, index) => {
+            return (
+              <div
+                key={index}
+                onClick={() => setUserInput((prev) => prev + letter)}
+                class="hive-cell"
+              >
+                {letter}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="btn-container">
+        <button className="btn" onClick={() => setUserInput("")}>
+          Delete
+        </button>
+        <button className="btn" onClick={() => setLetters(refresh(letters))}>Refresh</button>
+        <button className="btn">Enter</button>
+      </div>
+    </div>
+  );
+};
 
-export default Board
+export default Board;
